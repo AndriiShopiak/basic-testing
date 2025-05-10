f1a3d6cc2d9a1f4fb174f286c59680712e1e1fb5
Andrii Shopiak <andriyshopyak@gmail.com>
2025-05-10 10:26:22 +0300

    feat: add solution for error handling

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,68 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const value = 'test value';
+    const result = await resolveValue(value);
+    expect(result).toBe(value);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    const message = 'This is a test error';
+    try {
+      throwError(message);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      if (error instanceof Error) {
+        expect(error.message).toBe(message);
+      }
+    }
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    const defaultMessage = 'This is a default error message';
+    try {
+      throwError(defaultMessage);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      if (error instanceof Error) {
+        expect(error.message).toBe(defaultMessage);
+      }
+    }
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    try {
+      throwCustomError();
+    } catch (error) {
+      expect(error).toBeInstanceOf(MyAwesomeError);
+      if (error instanceof MyAwesomeError) {
+        expect(error.message).toBe('This is my awesome custom error!');
+      }
+    }
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    try {
+      await rejectCustomError();
+    } catch (error) {
+      expect(error).toBeInstanceOf(MyAwesomeError);
+      if (error instanceof MyAwesomeError) {
+        expect(error.message).toBe('This is my awesome custom error!');
+      }
+    }
   });
 });
